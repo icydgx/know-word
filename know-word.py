0e232c870dc2b70447df5af8c55fd9629ee3d060
irtg <mikhailcolesty@gmail.com>
2017-04-28 20:53:07 +0800

    update

--- know-word.py
+++ know-word.py
@@ -6,8 +6,8 @@ from nltk.corpus import wordnet as wn
 
 def rebort(filename):
     raw_datas = open(filename, 'r+')
-    sentence0 = str(raw_datas.read())
-    sentence = sentence0.lower()
+    sentence = str(raw_datas.read())
+    # sentence = sentence0.lower()
     tokens = word_tokenize(sentence)
     counts = Counter(tokens)
     d = []
@@ -15,15 +15,19 @@ def rebort(filename):
     words = re.findall(r'[A-Za-z]+', str(d))
     datas = open('words.txt', 'r+')
     text = datas.read()
+
+    i=0
     for x in words:
         y=str(wn.morphy(x))
         myMatch = re.search(y, text)
         if not(myMatch):
+            i=i+1
             print(y)
             datas.writelines(y + '\n')
+    print("unknow words number is",i)
     datas.close()
 
-sentence = str(input("please input file directory \n>"))
-# sentence='know-word.py'
+# sentence = str(input("please input file directory \n>"))
+sentence='know-word.py'
 rebort(sentence)
 
