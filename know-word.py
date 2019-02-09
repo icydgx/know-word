3885be3597e2e9eaab99d7d3a55607f83eabbf70
icydgx <mikhailcolesty@gmail.com>
2019-02-09 20:14:41 +0800

    format code

--- know-word.py
+++ know-word.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 import re
 from collections import Counter
-from nltk.tokenize import word_tokenize
+
 from nltk.corpus import wordnet as wn
 from nltk.stem import SnowballStemmer
+from nltk.tokenize import word_tokenize
+
 
 def rebort(filename):
     raw_datas = open(filename, 'r+')
@@ -18,17 +20,18 @@ def rebort(filename):
 
     snowball_stemmer = SnowballStemmer("english")
 
-    i=0
+    i = 0
     for x in words:
-        y=str(wn.morphy(x))
-        z=str(snowball_stemmer.stem(x))
+        y = str(wn.morphy(x))
+        z = str(snowball_stemmer.stem(x))
         yMatch = re.search(y, text)
         zMatch = re.search(z, text)
         if not yMatch and not zMatch:
-            i=i+1
+            i = i + 1
             print(x)
-            datas.writelines(y+'\n')
-    print("unknow words number is",i)
+            datas.writelines(y + '\n')
+
+# print("unknow words number is",i)
     datas.close()
 
 sentence = str(input("please input file directory \n>"))
