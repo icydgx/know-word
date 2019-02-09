f69349e193c3ae19f94c47bd0697174dada12370
icydgx <mikhailcolesty@gmail.com>
2019-02-10 00:31:17 +0800

    fix match None bug

--- know-word.py
+++ know-word.py
@@ -21,16 +21,26 @@ def rebort(filename):
     snowball_stemmer = SnowballStemmer("english")
 
     i = 0
+    b = 0
     for x in words:
         y = str(wn.morphy(x))
         z = str(snowball_stemmer.stem(x))
         yMatch = re.search(y, text)
         zMatch = re.search(z, text)
+        try:
+            if yMatch.group() == "None":
+                dMatch = re.search(x, text)
+                if not dMatch:
+                    i = i + 1
+                    print(x)
+            #     datas.writelines(x + '\n')
+        except AttributeError as t:
+            fklasdi = 1  #这里不知道写些什么
         if not yMatch and not zMatch:
             i = i + 1
             print(x)
             # datas.writelines(y + '\n')
-    print("unknow words number is", i)
+    print("unknown words number is", i)
     datas.close()
 
 
