16b2c929e7fb78b6647ac3885e0cba7e52bed4b8
irtg <mikhailcolesty@gmail.com>
2019-05-25 21:54:34 +0800

    rewrite

--- moviedubbed.py
+++ moviedubbed.py
@@ -1,86 +1,47 @@
 #!/usr/bin/python
 import re
-import nltk
-from collections import Counter
-from nltk.stem.wordnet import WordNetLemmatizer
+import pysrt
+import sys
+import argv
 
+# fileName= (argv.k(sys.argv[1:]))
+fileName='zm.srt'
+raw_datas= pysrt.open(fileName)
+sentence=''
+for line in raw_datas:
+    sentence=sentence+str(line)
 
-def rebort(filename):
-    #     sentence = """Take the airship back to where the tower is, and head north from
-    # there to find the Mana Tree where we really kill apples great great
-    # ha..."""
-    raw_datas = open(filename, 'r+',encoding='utf8')
-    sentence0 = str(raw_datas.read())
-    # print(sentence0)
-    # sentence0=sentence0.decode('utf-16')
-    # sentence0=sentence0.replace('\n', ' ')
-    # sentence0=re.search(r'[A-Za-z]+',sentence0)
-    # sentence0=sentence0.group()
-    # print(sentence0)
-    sentence = sentence0.lower()
-    lmtzr = WordNetLemmatizer()
-    # snowball_stemmer = SnowballStemmer("english")
-    tokens = nltk.word_tokenize(sentence)
-
-    # nonPunct=re.compile('.*[A-Za-z0-9].*')
-    # filtered=[w for w in tokens if nonPunct.match(w)]
-    counts = Counter(tokens)
-    d = []
-    d.append(counts)
-    words = re.findall(r'[A-Za-z]+', str(d))
-    # words[0]
-    # print(words[3])
-    datas = open('words.txt', 'r+',encoding='utf8')
-    text = datas.read()
-    for x in words:
-        # print(tokens[x])
-        y = lmtzr.lemmatize(x)
-        myMatch = re.search(y, text)
-        if not(myMatch):
-            # most have one sentence
-            # myMatch.group(0)
-            bool(re.search("\b{0}\b".format(x),text))
-            print(y)
-            datas.writelines(y + '\n')
-        # else:
-        #     print(x)
-        #     # print(y)
-        #     # regex = re.compile(r".*\(.*'+y+'.*[.,?!]*\)")
-        #     # result = regex.search(sentence0)
-        #     # print(sentence0)
-        #     # searchObj=re.search(r'.*[.](.*aoeu.*[.])',sentence)
-        #     searchObj = re.search(r'(.*' + y + '.*[.,])' + '.*', sentence)
-        #     if searchObj:
-
-        #         # searchObj=re.search(r'',sentence0)
-
-        #         print(searchObj.group(1))
-            # searchObj=re.search(r'(.*'+y+'.*[.,])'+'.*',sentence)
-            # if searchObj:
-
-            # # searchObj=re.searnch(r'',sentence0)
-
-            #     print(searchObj.group(1))
-
-            # my name is good.you will shoot by me!i like you
-            # good morning.i think emacs is great.i love you
-
-            # print(myMatch.group(0)
-            # )
-            # print(y)
-
-            # datas.writelines(y+'\n')
-        # words = re.findall(r"[\w]+",sentence)
-
-    datas.close()
-
-
-# sentence = str(input("请输入文档路径\n>"))
-# sentence='tmp.org'
-# sentence = '/home/czj/Downloads/resident.evil.the.final.chapter.(2016).eng.1cd.(6873925)/Resident Evil The Final Chapter 2017 HD-TS x264-CPG.srt'
-# sentence = './test.txt'
-# sentence='Desperate.Housewives.S03E09.Beautiful.Girls.720p.WEB-DL.AAC2.0.h.264-TjHD.ch-eng.srt'
-sentence='a.srt'
-# print(sentence)
-rebort(sentence)
-# /home/czj/Videos/Desperate.Housewives.S03.720p.WEB-DL.AAC2.0.h.264-TjHD/Desperate.Housewives.S03E09.Beautiful.Girls.720p.WEB-DL.AAC2.0.h.264-TjHD.ch-eng.srt
+datas = open('out.srt', 'r+')
+datas.truncate()
+sentenceSplit = filter(None, sentence.split("\n"))
+sentenceSplit2 = filter(None, sentence.split("\n"))
+a = 0
+b = 0
+unknown = open("temp.txt", 'r+')
+unknown_sentence = str(unknown.read())
+d = []
+words = re.findall(r'[A-Za-z]+', str(unknown_sentence))
+for t in sentenceSplit:
+    d.append(t)
+for s in sentenceSplit2:
+    k = re.findall(r'[A-Za-z]+', str(s))
+    if (k):
+        for y in words:
+            myMatch = re.search(y, s)
+            if (myMatch):
+                print(y)
+                b = 1
+        if b == 1:
+            datas.writelines(d[a - 3])
+            datas.writelines("\n")
+            datas.writelines(d[a - 2])
+            datas.writelines("\n")
+            datas.writelines(d[a - 1])
+            datas.writelines("\n")
+            datas.writelines(d[a])
+            datas.writelines("\n")
+            datas.writelines("\n")
+    a = a + 1
+    b = 0
+datas.close()
+unknown.close()
