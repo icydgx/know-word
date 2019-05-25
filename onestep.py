fa086d6a2f659e9c3d481904bc313125da0a0551
irtg <mikhailcolesty@gmail.com>
2019-05-25 21:57:42 +0800

    Add files via upload

---
+++ onestep.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import codecs
+import time
+import subprocess
+
+i=""                            # input srt name
+subprocess.call("python know-word.py -n %s" % (str(i)), shell=True)
+subprocess.call("python moviedubbed.py -n %s" % (str(i)), shell=True)
+print("run success")
