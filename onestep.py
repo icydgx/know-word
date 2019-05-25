45503a61e6489fcf08ef2fb8bebdc46dfc8ffb92
irtg <mikhailcolesty@gmail.com>
2019-05-25 21:59:28 +0800

    Update onestep.py

--- onestep.py
+++ onestep.py
@@ -6,5 +6,5 @@ import subprocess
 
 i=""                            # input srt name
 subprocess.call("python know-word.py -n %s" % (str(i)), shell=True)
-subprocess.call("python moviedubbed.py -n %s" % (str(i)), shell=True)
+subprocess.call("python movieDubbed.py -n %s" % (str(i)), shell=True)
 print("run success")
